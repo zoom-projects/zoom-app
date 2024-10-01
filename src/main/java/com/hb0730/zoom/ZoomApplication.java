81232975d38d073fb67621965614329f4560aa1b
hb0730 <1278032416@qq.com>
2024-10-01 13:46:44 +0800

    feat: init

---
+++ src/main/java/com/hb0730/zoom/ZoomApplication.java
@@ -0,0 +1,16 @@
+package com.hb0730.zoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author <a href="mailto:huangbing0730@gmail">hb0730</a>
+ * @date 2024/9/23
+ */
+@SpringBootApplication
+public class ZoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZoomApplication.class, args);
+    }
+}
